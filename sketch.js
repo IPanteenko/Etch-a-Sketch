72715e3512a7f9366489814e61accd5d27c86232
IPanteenko <panteenko@gmail.com>
2024-06-21 18:00:59 +1200

    added a user pop up

--- sketch.js
+++ sketch.js
@@ -1,31 +1,49 @@
 const grid = document.querySelector('.grid');
-let width;
-let height;
 
-function setGrid(width, height) {
-    for(let i=0; i<= height; i++ ) {
+let squares;
+
+function setGrid(squares) {
+    for (let i = 0; i <= squares; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
-        
-        for(let j=0; j<= width; j++) {
+
+        for (let j = 0; j <= squares; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
             row.appendChild(cell);
         }
         grid.appendChild(row);
     }
+    addCellHover();
 }
 
-setGrid(16,16);
+setGrid(16);
 
-
-const cell = document.querySelectorAll('.cell');
-
-for( let i=0; i< cell.length; i++) {
-    cell[i].addEventListener('mouseover', () => {
-        cell[i].style.backgroundColor = 'red';
-    })
+function removeGrid() {
+    const cell = document.querySelectorAll('.cell');
+    cell.forEach((e) => e.parentNode.removeChild(e));
 }
+
+const button = document.querySelector('#size');
+button.addEventListener('click', () => {
+    squares = prompt("Please, enter amount of squares per side for the new grid");
+    removeGrid();
+    setGrid(squares);
+})
+
+
+function addCellHover() {
+    const cell = document.querySelectorAll('.cell');
+
+    for (let i = 0; i < cell.length; i++) {
+        cell[i].addEventListener('mouseover', () => {
+            cell[i].style.backgroundColor = 'red';
+        })
+    }
+}
+
+
+
 
 
 
